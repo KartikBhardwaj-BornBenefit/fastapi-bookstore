c25c2dd886b8d44c88fb1dfa36f0007884b649b1
KartikBhardwaj-BornBenefit <kartikbhardwaj162522@gmail.com>
2025-07-27 03:13:30 +0530

    deployment.py

--- deployment.py
+++ deployment.py
@@ -24,16 +24,30 @@ def get_book(db:Session , book_id : int):
 def delete_book(db:Session , title:str):
     book = db.query(deployment_models.Book).filter(deployment_models.Book.title == title).first()
     if book:
-        db.delete(book)
+        db.delete(book)      
         db.commit()
         return book
 
+def get_db():
+    db = sessionlocal()
+    try:
+        yield db
+    finally:
+        db.close()    
+
 @app.post("/books" , response_model= deployment_schemas.BookOut)
-def create_book(db:Session , book: deployment_schemas.BookCreate):
+def create_book( book: deployment_schemas.BookCreate  ,db:Session = Depends(get_db)):
     db_book = deployment_models.Book(title = book.title , author = book.author)
     db.add(db_book)
     db.commit()
+    db.refresh(db_book)
     return db_book
+
+
+@app.get("/books", response_model= list[deployment_schemas.BookOut])
+def get_books(db:Session = Depends(get_db)):
+    return db.query(deployment_models.Book).all()
+
 
 
 Base.metadata.create_all(bind= engine)
